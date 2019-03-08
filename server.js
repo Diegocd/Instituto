eb7c837fc8a3c94e4fb699ee63341a0eae1967ca
Diego <prodigioso3axer@gmail.com>
2019-03-08 10:31:10 +0100

    Creación config.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-
+const config = require('./config');
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/ies', { useNewUrlParser: true })
+mongoose.connect(config.db_uri, { useNewUrlParser: true })
     .then(db => console.log('Conexión corrmodelscta a la BD'))
     .catch(err => console.log('Error en la conexión a la BD'));
 
@@ -17,6 +17,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use ('/api', routes);
 
-app.listen(3000, () => {
+app.listen(config.port, () => {
     console.log('Iniciando servidor en puerto 3000');
 });
